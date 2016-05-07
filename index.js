b6759fb677aa9bc9e6b9ea9626175a2b7879c2ee
Shahar Talmi <shahart@wix.com>
2016-05-07 23:58:21 +0300

    intial implementation

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  extends: 'xo',
+  env: {
+    protractor: true,
+    mocha: true
+  },
+  rules: {
+    indent: [2, 2, {
+      SwitchCase: 1
+    }],
+    'padded-blocks': 0,
+    'no-throw-literal': 0,
+    'no-else-return': 0,
+    'no-implicit-coercion': [2, {
+      boolean: false,
+      string: false
+    }],
+    'spaced-comment': 0,
+    'no-multiple-empty-lines': 0,
+    'one-var': 0,
+    'no-negated-condition': 0,
+    radix: 0,
+    'no-implicit-globals': 0,
+    'one-var-declaration-per-line': 0,
+    'max-nested-callbacks': 0,
+    'comma-dangle': [2, 'only-multiline'],
+    'quote-props': [2, 'as-needed'],
+    'no-bitwise': 2
+  }
+};
